374ae2c110c5e25636bdf5fd9532c118ae7906a3
thiago-anjos <thiago.w3c@gmail.com>
2020-02-25 18:36:25 -0300

    Updates

---
+++ precache-manifest.e1d4f9b0bf34461a3d408da51d9c4f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1b546e9d30db6e172ea6eaf47777fc",
+    "url": "/react-timer/index.html"
+  },
+  {
+    "revision": "6dc18dd68959580a23e0",
+    "url": "/react-timer/static/css/main.a565116c.chunk.css"
+  },
+  {
+    "revision": "5e7d6a1efcf8adafab3a",
+    "url": "/react-timer/static/js/2.56dbed30.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-timer/static/js/2.56dbed30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc18dd68959580a23e0",
+    "url": "/react-timer/static/js/main.acf393e8.chunk.js"
+  },
+  {
+    "revision": "e8599ee268ebbab9cbb0",
+    "url": "/react-timer/static/js/runtime-main.18890312.js"
+  },
+  {
+    "revision": "54825791bba9807d5e4975c702d4d4d0",
+    "url": "/react-timer/static/media/cronometro.54825791.png"
+  }
+]);
